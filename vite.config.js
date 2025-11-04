98ffd2436f85eef538ad1a76165e2c67e1364237
Ulisses Abreu <Ulisses.wrk@gmail.com>
2025-11-03 21:32:51 -0300

    fix: configura o roteamento pra vercel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Professional-Tech-Portfolio/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 });
